11c8cb5ebb2b7d0b87870bd96a253b1f631716ba
Amin <aminullahpuya@gmail.com>
2025-09-23 15:54:02 +0200

    brought some changes

--- main.py
+++ main.py
@@ -56,3 +56,10 @@ i = 0
 while i < len(fruits):
     print(fruits[i])
     i = i + 1
+
+print('-----Adding two tuples-----')
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
